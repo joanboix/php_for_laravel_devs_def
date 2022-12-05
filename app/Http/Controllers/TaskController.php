eccd56c8e2d87bc013990dc7ac8fb7610c4d2ce9
joanboix <joanboix@iesebre.com>
2022-12-05 15:49:16 +0100

    Refactoritzar la funció anònima que tenia a routes/web.php a un controlador.

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
 
+    public function index()
+    {
+        return view('tasks', [
+            'tasks' => Task::all()
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,42 +1,11 @@
 <?php
 
-use App\Models\Task;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-//    $task1 = Task::create([
-//        'title' => 'Tasca1',
-//        'description' => 'Tasca1 desc',
-//        'completed' => 0,
-//    ]);
-//
-//    $task2 = Task::create([
-//        'title' => 'Tasca2',
-//        'description' => 'Tasca2 desc',
-//        'completed' => 1,
-//    ]);
-//
-//    $task3 = Task::create([
-//        'title' => 'Tasca3',
-//        'description' => 'Tasca3 desc',
-//        'completed' => 0,
-//    ]);
-//
-//    $tasks = [
-//        $task1,
-//        $task2,
-//        $task3
-//    ];
-    return view('tasks',[
-        'tasks' => Task::all()
-    ]);
-});
+Route::get('/', [TaskController::class,'index']);
 
-Route::get('/tasks', function () {
-    return view('tasks',[
-        'tasks' => []
-    ]);
-});
+Route::get('/tasks', [TaskController::class,'index']);
 
 Route::get('/users', function () {
     return view('users');
