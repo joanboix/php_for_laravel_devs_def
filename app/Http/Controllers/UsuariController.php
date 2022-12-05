03c1057d2ed345464d741f84944fc1170ca246fb
joanboix <joanboix@iesebre.com>
2022-12-05 16:44:39 +0100

    Crear controladors per a les diferents pàgines. Crear Models per a Usuari i implementar-ho tot de forma correcta.

---
+++ app/Http/Controllers/UsuariController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Usuari;
+use Illuminate\Http\Request;
+
+class UsuariController extends Controller
+{
+
+    public function index()
+    {
+        return view('users', [
+            'usuaris' => Usuari::all()
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,17 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UsuariController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [TaskController::class,'index']);
 
 Route::get('/tasks', [TaskController::class,'index']);
 
-Route::get('/users', function () {
-    return view('users');
-});
+Route::get('/users', [UsuariController::class,'index']);
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::get('/contact', [ContactController::class,'index']);
 
-
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/about', [AboutController::class,'index']);
